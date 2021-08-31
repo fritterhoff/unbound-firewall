2ed4ce8b18413b54685475069b8fd8b63295a70f
Florian Ritterhoff <florian@ritterhoff.org>
2021-08-31 08:35:36 +0200

    Update downloader script

--- downloader/download.py
+++ downloader/download.py
@@ -12,8 +12,8 @@ import socket
 
 class UnboundDownloader(object):
     __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    
-    unbound_conf_dir = '/etc/unbound/'
+
+    unbound_conf_dir = "/etc/unbound/"
     ip_range_lists = {}
     categories = {}
     category_domains = {}
@@ -27,7 +27,7 @@ class UnboundDownloader(object):
             return None, False
 
         # Split the lines at spaces, tabs, ... and handle a prefixed ip adress
-        parts = re.split(r"\s", domain)
+        parts = re.split(r"\s+", domain)
         if len(parts) >= 2:
             if re.match(self.ip_regex, parts[0]):
                 domain = parts[1]
@@ -35,7 +35,7 @@ class UnboundDownloader(object):
         # Remove trailing comments
         domain = domain.split("#")[0]
         if len(domain) != 0:
-            return domain.strip(), True
+            return domain.strip().lstrip('.'), True
         return None, False
 
     def _format_ipv4(self, ip_range: bytes) -> (str, bool):
@@ -49,7 +49,7 @@ class UnboundDownloader(object):
             parts = re.split(r"[./]", ip_range)
             data = ""
             for idx in range(len(parts)):
-                item = parts[len(parts)-idx-1]
+                item = parts[len(parts) - idx - 1]
                 data += item + "."
             data += "rpz-ip"
             return data, True
@@ -63,13 +63,13 @@ class UnboundDownloader(object):
         ip_range = ip_range.split(";")[0].strip()
         try:
             _ = ipaddress.ip_network(ip_range)
-            parts = ip_range.split('/')
+            parts = ip_range.split("/")
 
             data = parts[1]
-            parts[0] = parts[0].replace('::', ':zz:')
-            parts = parts[0].split(':')
+            parts[0] = parts[0].replace("::", ":zz:")
+            parts = parts[0].split(":")
             for idx in range(len(parts)):
-                item = parts[len(parts)-idx-1]
+                item = parts[len(parts) - idx - 1]
                 if len(item) != 0:
                     data += item
                 data += "."
@@ -88,48 +88,48 @@ class UnboundDownloader(object):
 
     def prepareDatasets(self):
 
-        source, self.categories, self.ip_range_lists = self._read_providers(
-            os.path.join(self.__location__, "providers.json"))
+        source, self.categories, self.ip_range_lists = self._read_providers(os.path.join(self.__location__, "providers.json"))
 
         lists = requests.get(source).iter_lines()
         for item in lists:
             csv_reader = csv.reader([str(item, "utf-8")])
             for row in csv_reader:
-                if row[1] == 'tick':
+                if row[1] == "tick":
                     for _, data in self.categories.items():
-                        if data['description'] == row[0]:
-                            if not 'providers' in data:
-                                data['providers'] = []
-                            data['providers'].append(row[4])
+                        if data["description"] == row[0]:
+                            if "providers" not in data:
+                                data["providers"] = []
+                            data["providers"].append(row[4])
                             break
 
         for category_id, data in self.categories.items():
             parsedDomains = set()
             self.category_domains[category_id] = set()
             for provider in data["providers"]:
-                domains = requests.get(provider).iter_lines()
-                for domain in domains:
-                    domain, ok = self._format_domain(domain)
-                    if ok:
-                        parsedDomains.add(domain)
+                try:
+                    domains = requests.get(provider).iter_lines()
+                    for domain in domains:
+                        domain, ok = self._format_domain(domain)
+                        if ok:
+                            parsedDomains.add(domain)
+                except Exception:
+                    print(f"Handling {provider} failed")
             parsedDomains = sorted(parsedDomains)
-            target = self.categories[category_id]['target']
+            target = self.categories[category_id]["target"]
             for domain in parsedDomains:
-                self.category_domains[category_id].add(f'{domain} CNAME {target}')
+                self.category_domains[category_id].add(f"{domain} CNAME {target}")
 
-        with open(os.path.join(self.__location__,"./zone_template")) as file:
-            self.header = file.read().replace("{SERIAL}", datetime.datetime.now().strftime(
-                "%Y%m%d%H")).replace("{HOSTNAME}", socket.getfqdn())
+        with open(os.path.join(self.__location__, "./zone_template")) as file:
+            self.header = file.read().replace("{SERIAL}", datetime.datetime.now().strftime("%Y%m%d%H")).replace("{HOSTNAME}", socket.getfqdn())
 
     def domain_data(self):
         for category_combination in range(len(self.categories)):
-            description = self.categories[str(category_combination)]['description']
+            description = self.categories[str(category_combination)]["description"]
 
             file_header = self.header.replace("{ZONE}", f"{description}")
-            description = str.lower(description).replace(' ', '-')
+            description = str.lower(description).replace(" ", "-")
             file_path = os.path.join(self.unbound_conf_dir, f"rpz.{description}.zone")
-            domains = "\n".join(
-                self.category_domains[str(category_combination)]) + "\n"
+            domains = "\n".join(self.category_domains[str(category_combination)]) + "\n"
 
             # Ensure that the content is different. Otherwise changes can be prevented.
             if os.path.exists(file_path):
@@ -137,8 +137,7 @@ class UnboundDownloader(object):
                     present = file.read()
                     present = present[len(file_header):]
                     if present == domains:
-                        print(
-                            f'Content of type {description} is unchanged --> Leaving file unchanged')
+                        print(f"Content of type {description} is unchanged -> Leaving file unchanged")
                         continue
 
             with open(file_path, "w") as file:
@@ -162,8 +161,7 @@ class UnboundDownloader(object):
 
             maxLength = len(max(ips, key=len))
             for ip in ips:
-                file.write(
-                    f'{ip:{maxLength}} CNAME {self.ip_range_lists["target"]}\n')
+                file.write(f'{ip:{maxLength}} CNAME {self.ip_range_lists["target"]}\n')
 
 
 if __name__ == "__main__":
